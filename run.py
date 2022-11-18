7082390254ff573518c510941250d075b195ed2e
aboczek <adam.boczekwork@gmail.com>
2022-11-18 18:14:33 +0000

    defining nickname function to take user input for nickname

--- run.py
+++ run.py
@@ -1,11 +1,22 @@
 import random
-from art import *
+from art import tprint
 import gspread
 
 word = ["test"]
-guess_mistakes = 7
-
+user_nickname = []
+GUESS_MISTAKES = 7
 
 tprint("Welcome")
 tprint("To")
 tprint("Hangman")
+
+
+def nickname():
+    """
+    Takes users nickname and displays it
+    """
+    user_input = input("Put your nickname here: ")
+    print(f"Your nickname is {user_input}.")
+
+
+nickname()
