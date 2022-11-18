97d6cfe30faa24686ca46617a6912a2e37973938
aboczek <adam.boczekwork@gmail.com>
2022-11-18 17:55:38 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from art import *
+import gspread
+
+word = ["test"]
+guess_mistakes = 7
+
+
+tprint("Welcome")
+tprint("To")
+tprint("Hangman")
