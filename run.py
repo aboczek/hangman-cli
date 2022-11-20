6f5f6eb1044c0ad5a4e20b246361760988dbee97
aboczek <adam.boczekwork@gmail.com>
2022-11-20 21:24:38 +0000

    adding scope and code for google drive/google sheets apis

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
 import random
+import time
 from art import tprint
 import gspread
-import time
+from google.oauth2.service_account import Credentials
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open("hangman")
 
 
 dicto = ["house", "leave", "letter", "Python", "Java"]
